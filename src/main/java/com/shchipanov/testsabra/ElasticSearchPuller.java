12abc7d174a7ad0610f1fc34e064be85133b06d4
Guuugol <abcd666@bk.ru>
2019-03-18 14:43:07 +0200

    trying to add elastic

--- src/main/java/com/shchipanov/testsabra/ElasticSearchPuller.java
+++ src/main/java/com/shchipanov/testsabra/ElasticSearchPuller.java
@@ -1,20 +1,30 @@
 package com.shchipanov.testsabra;
 
+import org.elasticsearch.action.bulk.BulkRequestBuilder;
+import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.TransportAddress;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
 import org.springframework.stereotype.Component;
 
-import javax.ws.rs.core.Response;
 import java.net.InetAddress;
 import java.util.List;
+
 
 @Component
 public class ElasticSearchPuller {
 
-//    public Response pullData(List<GoogleSearchResultItem> data) {
-//        Client client = new PreBuiltTransportClient();
-//
-//
-//    }
+    public void pullData(List<GoogleSearchResultItem> data) {
+        Client client = new PreBuiltTransportClient(
+                Settings.builder().put("client.transport.sniff", true)
+                        .put("cluster.name","elasticsearch").build())
+                .addTransportAddress(new TransportAddress(InetAddress.getByName("127.0.0.1"), 9300));
+
+        BulkRequestBuilder bulkRequest = client.prepareBulk();
+
+        bulkRequest.add(new IndexRequest("results").id("1")
+                .source(XContentType.values(), data));
+    }
 }

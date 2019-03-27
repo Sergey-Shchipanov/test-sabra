183c5ad0530d194d69482f59b549ed68b36cde4f
Guuugol <abcd666@bk.ru>
2019-03-27 13:04:51 +0200

    creat index

--- src/main/java/com/shchipanov/testsabra/ElasticSearchPuller.java
+++ src/main/java/com/shchipanov/testsabra/ElasticSearchPuller.java
@@ -1,9 +1,11 @@
 package com.shchipanov.testsabra;
 
 import lombok.extern.slf4j.Slf4j;
+import org.elasticsearch.action.admin.indices.create.CreateIndexRequest;
 import org.elasticsearch.action.bulk.BulkRequest;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.client.Client;
+import org.elasticsearch.client.IndicesAdminClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.TransportAddress;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
@@ -11,7 +13,9 @@ import org.springframework.stereotype.Component;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 
@@ -20,20 +24,36 @@ import java.util.UUID;
 public class ElasticSearchPuller {
 
     public void pullData(List<GoogleSearchResultItem> data) throws UnknownHostException {
-            Client client = new PreBuiltTransportClient(
-                    Settings.builder().put("cluster.name", "docker-cluster")
-                            .build())
-                    .addTransportAddress(new TransportAddress(InetAddress.getByName(System.getProperty("sabra.elastic.ip")), 9300));
+        Client client = new PreBuiltTransportClient(
+                Settings.builder().put("cluster.name", "docker-cluster")
+                        .build())
+                .addTransportAddress(new TransportAddress(InetAddress.getByName(System.getProperty("sabra.elastic.ip")), 9300));
 
-            BulkRequest bulkRequest = new BulkRequest();
+        IndicesAdminClient indicesAdminClient = client.admin().indices();
+        CreateIndexRequest request = new CreateIndexRequest("results");
 
-            data.forEach(res -> bulkRequest.add(
-                    new IndexRequest("results").id(String.valueOf(UUID.randomUUID()))
-                            .source("title", res.getTitle())
-                            .source("link", res.getLink())
-                            .source("cacheId", res.getCacheId())
-                            .source("displayLink", res.getDisplayLink())));
 
-            client.bulk(bulkRequest);
+        Map<String, Object> jsonMap = new HashMap<>();
+        Map<String, Object> mapping = new HashMap<>();
+        mapping.put("id", "id");
+        mapping.put("title", "title");
+        mapping.put("link", "link");
+        mapping.put("cacheId", "cacheId");
+        mapping.put("displayLink", "displayLink");
+        jsonMap.put("_doc", mapping);
+        request.mapping("_doc", jsonMap);
+
+        indicesAdminClient.create(request);
+
+        BulkRequest bulkRequest = new BulkRequest();
+
+        data.forEach(res -> bulkRequest.add(
+                new IndexRequest("results").id(String.valueOf(UUID.randomUUID()))
+                        .source("title", res.getTitle())
+                        .source("link", res.getLink())
+                        .source("cacheId", res.getCacheId())
+                        .source("displayLink", res.getDisplayLink())));
+
+        client.bulk(bulkRequest);
     }
 }
